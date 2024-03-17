4e32380e03d23bb09b2d7de857453ed69b7fd1ce
ranggachiko <ranggaryantico19@gmail.com>
2024-03-17 17:46:06 +0700

    upload all file

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan</h1>";
+        echo" Nama saya Rangga Ryantico, Saya tinggal di Bekasi, Olahraga yang saya gemari adalah Badminton";
+    }
+}
+?>
