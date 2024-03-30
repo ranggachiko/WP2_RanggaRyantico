8a8e22bbdbe7b11b39c444be5927c71742835c38
ranggachiko <ranggaryantico19@gmail.com>
2024-03-31 04:44:20 +0700

    web, v_index, v_header, v_footer

---
+++ application/views/v_header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebPro II | Merancang Template Sederhana dengan Codeigniter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/stylebuku.css">
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <hgroup>
+                <h1>RentalBuku.net</h1>
+                <h3>Membuat Template Sederhana dengan Codeigniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url(). 'index.php/web'?>">Home</a></li>
+                    <li><a href="<?php echo base_url(). 'index.php/web/about'?>">About</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
+    </div>
+</body>
+</html>
