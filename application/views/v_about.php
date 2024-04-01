cc655fb278ec8efa26b10f04562bc2740d23092f
ranggachiko <ranggaryantico19@gmail.com>
2024-04-02 00:11:45 +0700

    add v_about.php

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rangga</li>
+        <li>Nama Belakang : Ryantico</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Andara Samping Rumah Raffi Ahmad No. 19 Depok</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Seattle, USA</li>
+    </ul>
+
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Badminton</li>
+        <li>Renang</li>
+    </ul>
+</section>
