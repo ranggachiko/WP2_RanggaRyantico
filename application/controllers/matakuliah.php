cfeb0ad7b7fd350b8ee953eb40f7eb543d9a6b14
ranggachiko <ranggaryantico19@gmail.com>
2024-04-05 14:38:24 +0700

    modified autoload.php and matakuliah.php

--- application/controllers/matakuliah.php
+++ application/controllers/matakuliah.php
@@ -8,12 +8,28 @@ class matakuliah extends CI_Controller
 
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks')
+        $this->form_validation->set_rules('kode','kode matakuliah',
+        'required|min_length[3]',[
+            'required' => 'Kode Matakuliah Harus Diisi',
+            'min_length' => 'Kode Terlalu Pendek'
+        ]);
+
+        $this->form_validation->set_rules('nama','nama matakuliah',
+        'required|min_length[3]',[
+            'required' => 'Nama Matakuliah Harus Diisi',
+            'min_length' => 'Nama Terlalu Pendek'
+        ]);
+
+        if ($this->form_validation->run() != true){
+            $this->load->view('view-form-matakuliah');
+        } else{
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks')
         ];
 
         $this->load->view('view-data-matakuliah', $data);
+       }
     }
 }
